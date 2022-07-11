1fe938acd715dfd8b1b8fd8fe2f9eb13202aa2d4
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2022-07-11 09:09:24 -0400

    Projeto criado.

---
+++ backend/src/main/java/com/danielpg/dgmeta/DgmetaApplication.java
@@ -0,0 +1,13 @@
+package com.danielpg.dgmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgmetaApplication.class, args);
+	}
+
+}
