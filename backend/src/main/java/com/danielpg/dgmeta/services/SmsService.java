6987b9212e06e4c2deeb95fdf040c67020ec9e36
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2022-07-13 10:26:47 -0400

    Envio de SMS usando Twilio.

---
+++ backend/src/main/java/com/danielpg/dgmeta/services/SmsService.java
@@ -0,0 +1,50 @@
+package com.danielpg.dgmeta.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.danielpg.dgmeta.entities.Sale;
+import com.danielpg.dgmeta.repositories.SaleRepository;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+@Service
+public class SmsService {
+
+	@Value("${twilio.sid}")
+	private String twilioSid;
+
+	@Value("${twilio.key}")
+	private String twilioKey;
+
+	@Value("${twilio.phone.from}")
+	private String twilioPhoneFrom;
+
+	@Value("${twilio.phone.to}")
+	private String twilioPhoneTo;
+	
+	@Autowired
+	private SaleRepository saleRepository;
+
+	public void sendSms(Long saleId) {
+		
+		Sale sale = saleRepository.findById(saleId).get();
+
+		String msg =
+			"Venda ID: " + sale.getId().toString() +
+			"\nVendedor " + sale.getSellerName() +
+			"\nData: " + sale.getDate().toString() + 
+			"\nTotal: " + String.format("%.2f", sale.getAmount());
+
+		Twilio.init(twilioSid, twilioKey);
+
+		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+		Message message = Message.creator(to, from, msg).create();
+
+		System.out.println(message.getSid());
+	}
+}
